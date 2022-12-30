6352a1efe31c2378e562a4b2fb8b405fa67d756d
LEMSantos <lu.casmou.ra177833@gmail.com>
2022-12-30 12:41:07 -0300

    feat: exporta as classes relacionadas ao mapa

---
+++ src/map/__init__.py
@@ -0,0 +1,14 @@
+from src.map.generic_sprite import GenericSprite
+from src.map.wild_flower import WildFlower
+from src.map.water import Water
+from src.map.tree import Tree
+from src.map.interaction import Interaction
+from src.map.map_layers import MAP_LAYERS
+
+__all__ = (
+    "GenericSprite",
+    "WildFlower",
+    "Water",
+    "Tree",
+    "Interaction"
+)
