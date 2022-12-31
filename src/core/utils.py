dfc2afa9b20903ad88d8efcecb94ce16f4697468
LEMSantos <lu.casmou.ra177833@gmail.com>
2022-12-30 23:26:33 -0300

    feat: adiciona lógica para importar os arquivos também como um dicionário

--- src/core/utils.py
+++ src/core/utils.py
@@ -2,13 +2,24 @@ import os
 import pygame
 
 
-def import_folder(path):
-    surface_list = []
+def import_folder(path, get_dict=False):
+    def __handle_dict(surfaces, name, surf):
+        surfaces[name] = surf
+
+    def __handle_list(surfaces, _, surf):
+        surfaces.append(surf)
+
+    surfaces, handle_add = (
+        ({}, __handle_dict)
+        if get_dict else ([], __handle_list)
+    )
 
     for _, __, img_files in os.walk(path):
         for image in sorted(img_files):
-            surface_list.append(
+            handle_add(
+                surfaces,
+                image.replace(".png", ""),
                 pygame.image.load(f"{path}/{image}").convert_alpha()
             )
 
-    return surface_list
+    return surfaces
